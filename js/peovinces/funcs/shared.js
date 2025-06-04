ab3bcb101b4d1990146860dd40ef4c9c92a04627
Hossein <hosein.ghanimati1398@gmail.com>
2025-06-04 14:12:41 +0330

    handle deleting all provinces

--- js/peovinces/funcs/shared.js
+++ js/peovinces/funcs/shared.js
@@ -1,13 +1,12 @@
 import { getTrInfo, select, selectAll } from "../../../utils/elem.js";
 import { deleteReq, fetchProvinces, insertProvinces } from "./utils.js";
 
-const createModalEl = select(".province-create-modal")
+const createModalEl = select(".province-create-modal");
 export function showProvinceCreateModal() {
   createModalEl.classList.add("show");
 }
 
-
-const editModalEl = select(".province-edit-modal")
+const editModalEl = select(".province-edit-modal");
 export function showProvinceEditModal(id) {
   editModalEl.classList.add("show");
 }
@@ -18,27 +17,40 @@ export function closeProvinceModals() {
 }
 
 export function deleteProvince(event) {
-  const trInfo = getTrInfo(event)
-  swal(
-    `آیا شهر ${trInfo.name} حذف شود؟`
-  ).then( async result => {
-    if (result) {
-      try {
-        const response =  deleteReq(trInfo.id)
-        swal(response, "", "success")
-      } catch (error) {
-        swal("خطا در حذف شهر", error.message, "error")
+  const trInfo = getTrInfo(event);
+  swal(`آیا شهر ${trInfo.name} حذف شود؟`, "", "question").then(
+    async (result) => {
+      if (result) {
+        try {
+          const response = deleteReq(trInfo.id);
+          swal(response, "", "success");
+        } catch (error) {
+          swal("خطا در حذف شهر", error.message, "error");
+        }
       }
     }
-  })
+  );
 }
 
 export function deleteSelectedprovinces() {
   const selected = selectAll(".row-checkbox:checked");
   if (!selected.length) return swal("هیچ آیتمی انتخاب نشده است", "", "warning");
-  if (confirm("همه موارد انتخاب‌شده حذف شوند؟")) {
-    selected.forEach((cb) => cb.closest("tr").remove());
-  }
+  swal(`آیا ${selected.length} شهر انتخاب شده حذف شوند؟`, "", "question").then(
+    async (result) => {
+      if (result) {
+        try {
+          const infos = [...selected].map(
+            (cb) => JSON.parse(cb.closest("tr").dataset.info).id
+          );
+          const promices = infos.map((id) => deleteReq(id));
+          await Promise.all(promices);
+          swal("شهرها با موفقیت حذف شدند", "", "success");
+        } catch (error) {
+          swal("خطا در حذف شهر", error.message, "error");
+        }
+      }
+    }
+  );
 }
 
 export const renderProcinces = async () => {
